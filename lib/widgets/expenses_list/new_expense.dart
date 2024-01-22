22523caa3ebba364c6044d07aeea9476463b6c31
Parth Nepalia <parth.nepalia@gmail.com>
2024-01-22 12:27:20 +0530

    added error handling

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -14,16 +15,50 @@ class _NewExpenseState extends State<NewExpense> {
       TextEditingController(); // it is a class provided by flutter
   final _amountcontroller = TextEditingController();
 
-  void _presentDatePicker() {
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+  void _presentDatePicker() async {
     // date picker function , show date picker is inbuilt.
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
     final lastDate = now;
-    showDatePicker(
+    final pickedDate = await showDatePicker(
         context: context,
         initialDate: now,
         firstDate: firstDate,
         lastDate: lastDate);
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(
+        _amountcontroller.text); // tryParse('hello)=> null, tryParse('10')=> 10
+
+    final invalidAmount = enteredAmount == null || enteredAmount <= 0;
+    if (_titlecontroller.text.trim().isEmpty ||
+        invalidAmount ||
+        _selectedDate == null) {
+      // trim removes whitespace
+      //show error
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Invalid Input'),
+                content: const Text(
+                    'Please make sure a valid Title,amount,Date and cateogory was entered.'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(ctx);
+                      },
+                      child: const Text('Ok'))
+                ],
+              ));
+      return; // no code after this will be executed if control comes to the if statement.
+    }
   }
 
   @override
@@ -65,7 +100,10 @@ class _NewExpenseState extends State<NewExpense> {
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text('Selected Date'),
+                Text(_selectedDate == null
+                    ? 'No Selected Date'
+                    : formatter.format(
+                        _selectedDate!)), //! is added to force dart to assume that _selectedDate wont be null
                 IconButton(
                     onPressed: _presentDatePicker,
                     icon: const Icon(Icons.calendar_month)),
@@ -75,16 +113,31 @@ class _NewExpenseState extends State<NewExpense> {
         ),
         Row(
           children: [
+            DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map((category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase())))
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                }),
+            const SizedBox(
+              height: 16,
+            ),
             TextButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
                 child: const Text('Cancel')),
             ElevatedButton(
-              onPressed: () {
-                print(_titlecontroller.text);
-                print(_amountcontroller.text);
-              },
+              onPressed: _submitExpenseData,
               child: const Text('Save'),
             ),
           ],
