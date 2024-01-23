b478f2366036abe56b4bceeee305fdbb22b71aff
Parth Nepalia <parth.nepalia@gmail.com>
2024-01-23 12:27:53 +0530

    added dissmissal of expenses on swiping

--- lib/widgets/expenses_list/expenses.dart
+++ lib/widgets/expenses_list/expenses.dart
@@ -27,9 +27,22 @@ class _ExpensesState extends State<Expenses> {
 
   void _openExpenseList() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
   }
 
   @override
@@ -51,7 +64,11 @@ class _ExpensesState extends State<Expenses> {
         body: Column(
           children: [
             const Text('Chart'),
-            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+            Expanded(
+                child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            )),
           ],
         ));
   }
--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -59,6 +61,12 @@ class _NewExpenseState extends State<NewExpense> {
               ));
       return; // no code after this will be executed if control comes to the if statement.
     }
+    widget.onAddExpense(Expense(
+        title: _titlecontroller.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory));
+    Navigator.pop(context);
   }
 
   @override
@@ -71,7 +79,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(children: [
         TextField(
           controller: _titlecontroller,
